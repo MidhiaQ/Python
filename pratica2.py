028f221768462a72fbd7ec4798ed23ef377bc465
Midhia Queiroz <129337216+MidhiaQ@users.noreply.github.com>
2023-08-06 00:51:33 -0300

    Create pratica2.py

---
+++ pratica2.py
@@ -0,0 +1,9 @@
+#Faça um programa que solicite o salário de três servidoras e mostre a média de salário deles.
+
+salario1 = float(input('Informe o primeiro salario:'))
+salario2 = float(input('Informe o segundo salario:'))
+salario3 = float(input('Informe o segundo salario'))
+
+media = ((salario1 + salario2 + salario3)/3)
+
+print("A media dos salarios e: ", media)

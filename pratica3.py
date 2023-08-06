c82479963f8cd8c3ed2d6fc9247d4922044f7613
Midhia Queiroz <129337216+MidhiaQ@users.noreply.github.com>
2023-08-06 00:52:03 -0300

    Create pratica3.py

---
+++ pratica3.py
@@ -0,0 +1,7 @@
+#Faça um programa que solicite o raio de um círculo, calcule e mostre sua área (A = π * r 2 , considere π = 3.14)
+
+raio = float(input('Informe o raio do circulo:'))
+
+area = (3.14 * (raio ** 2))
+
+print('A area é:', area)

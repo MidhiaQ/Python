380bd372b013516784d808699927f0f6358187d9
Midhia Queiroz <129337216+MidhiaQ@users.noreply.github.com>
2023-08-06 00:52:30 -0300

    Create pratica4.py

---
+++ pratica4.py
@@ -0,0 +1,7 @@
+#Faça um programa que calcule a área de um quadrado, em seguida mostre seu resultado (A=L 2 ).
+
+lado = float(input('Informe o lado do quadrado:'))
+
+cal = (lado ** 2)
+
+print('A area do quadrado e:', cal)

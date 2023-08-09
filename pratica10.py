5b684b2a5b299e95fecc541f4bf6e1517e0d936a
Midhia Queiroz <129337216+MidhiaQ@users.noreply.github.com>
2023-08-09 15:11:07 -0300

    Create pratica10.py

---
+++ pratica10.py
@@ -0,0 +1,9 @@
+#1. Uma servidora do Ibama constatou que em uma determinada área de 35.000 ha,
+#foram desmatados 700 ha. Faça um programa que imprima na tela o percentual de desmatamento.
+
+area = 35000
+desmatado = 700
+
+percen = (desmatado/area)*100
+
+print(f"O percentual desmatado e {percen}%")

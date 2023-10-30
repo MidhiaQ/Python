694441571c92710ab679a694ba51006448bee33e
Midhia Queiroz <129337216+MidhiaQ@users.noreply.github.com>
2023-10-30 19:52:52 -0300

    Create Atividade1.py

---
+++ Atividade1.py
@@ -0,0 +1,12 @@
+#Faça um formulario que pergunte o nome, cpf, endereço, idade, altura e telefone
+#e imprima isso em um relatorio organizado.
+
+nome = input("Nome: ")
+cpf = input("CPF: ")
+ende = input("Endereço: ")
+ida = input("Idade: ")
+altura = input("Altura: ")
+tel = input("Telefone: ")
+print("Formulario")
+print("Nome: ", nome, "\nCPF: " ,cpf, "\nEndereço: " ,ende, "\nIdade: " ,ida, "\nAltura " ,altura, "\nTel: " ,tel)
+
